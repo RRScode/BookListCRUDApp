f12ff69c7467c3e88923d42460388340478d99af
rrscode <ricosantos86@gmail.com>
2024-01-30 12:54:33 -0800

    backend: updated CORS to 5173

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBooks';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
+
 
 const App = () => {
   return (
-    <div className = "bg-red-400 text-white">App</div>
+    <Routes>
+      <Route path='./pages/Home' element={} />
+      <Route path='' element={} />
+      <Route path='' element={} />
+      <Route path='' element={} />
+      <Route path='' element={} />
+    </Routes>
   )
 }
 
